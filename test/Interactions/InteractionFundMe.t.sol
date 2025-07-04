0647d9e6847620f357db836217c2576ab8bdbd66
shaah1d <shaahid2453@gmail.com>
2025-07-04 06:25:30 -0700

    withdraw test failed

--- test/Interactions/InteractionFundMe.t.sol
+++ test/Interactions/InteractionFundMe.t.sol
@@ -8,32 +8,38 @@ import {MockV3Aggregator} from "../mocks/MockAgreegatorv3.sol";
 import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 contract InteractionFundMe is Test {
+    uint256 public constant SEND_VALUE = 10e18;
     FundMe fundMe;
     address USER = makeAddr("user");
     MockV3Aggregator mockPriceFeed;
 
     uint256 public constant GAS_PRICE = 1;
 
+    receive() external payable {}
+
     function setUp() external {
         mockPriceFeed = new MockV3Aggregator(8, 2000e8);
-
         fundMe = new FundMe(address(mockPriceFeed));
-
         vm.deal(USER, 10 ether);
     }
 
-    function testUserCanFundInteractions() public {
-        FundFundMe fundFundMe = new FundFundMe();
-        
-        vm.deal(USER, 5e18);
-        vm.prank(USER);
-        fundFundMe.fundFundMe(address(fundMe));
+    function testUserCanFundAndOwnerWithdraw() public {
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundMe.getOwner()).balance;
+        uint256 originalFundMeBalance = address(fundMe).balance;
 
-        // address funder = fundMe.getFunder(0);
-        // assertEq(funder, USER);
+        // Using vm.prank to simulate funding from the USER address
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
         WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
         withdrawFundMe.withdrawFundMe(address(fundMe));
 
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundMe.getOwner()).balance;
+
         assert(address(fundMe).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(preOwnerBalance + SEND_VALUE + originalFundMeBalance, afterOwnerBalance);
     }
 }
